29fb747c5cf9b7c2ef414d340aa74648eaeabc67
Shrey <shrey.ghildiyal314@gmail.com>
2024-04-11 11:26:15 +0530

    drawing a line

--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"image/color"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/vector"
+)
+
+type Game struct{}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, "Hello, World!")
+	// fmt.Println("Draw called")
+
+	vector.StrokeLine(screen, 100, 100, 200, 200, 5, color.RGBA{
+		R: 200,
+		G: 100,
+		B: 0,
+		A: 255,
+	}, false)
+
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return 640, 480
+}
 
 func main() {
-	fmt.Println("Hello World!!")
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("Hello, World!")
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
 }
