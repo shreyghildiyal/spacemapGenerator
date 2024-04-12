33f7aa2724a83d6b67eaa8ce62f58ee7e431d345
Shrey <shrey.ghildiyal314@gmail.com>
2024-04-12 22:31:29 +0530

    switching to all lower case for package name

--- main.go
+++ main.go
@@ -1,16 +1,18 @@
 package main
 
 import (
-	"log"
-
 	"image/color"
+	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"github.com/hajimehoshi/ebiten/v2/vector"
+	mapgen "github.com/shreyghildiyal/spacemapGenerator/mapGen"
 )
 
-type Game struct{}
+type Game struct {
+	stars []mapgen.Location
+}
 
 func (g *Game) Update() error {
 	return nil
@@ -18,25 +20,54 @@ func (g *Game) Update() error {
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	ebitenutil.DebugPrint(screen, "Hello, World!")
-	// fmt.Println("Draw called")
 
-	vector.StrokeLine(screen, 100, 100, 200, 200, 5, color.RGBA{
+	var RED = color.RGBA{
 		R: 200,
-		G: 100,
+		G: 0,
 		B: 0,
-		A: 255,
-	}, false)
+		A: 0,
+	}
+
+	for _, star := range g.stars {
+		if star.IsClusterCore {
+			// vector.DrawFilledCircle(screen, float32(star.X), float32(star.Y), 2, RED, true)
+			vector.DrawFilledRect(screen, float32(star.X)-4, float32(star.Y)-4, 8, 8, RED, true)
+		} else {
+			vector.DrawFilledCircle(screen, float32(star.X), float32(star.Y), 2, color.White, true)
+		}
+
+	}
 
 }
 
+const HEIGHT = 800
+const WIDTH = 800
+
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 640, 480
+	return WIDTH, HEIGHT
 }
 
 func main() {
-	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowSize(WIDTH, HEIGHT)
 	ebiten.SetWindowTitle("Hello, World!")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+
+	game := Game{}
+
+	stars, _, err := mapgen.InitMap(mapgen.MapGenConfigs{
+		MaxX:                    WIDTH,
+		MaxY:                    HEIGHT,
+		StarCount:               600,
+		ClusterCount:            20,
+		StartRepulsionFactor:    50,
+		ClusterAttractionFactor: 0.95,
+	})
+	if err != nil {
+		log.Fatal("There was an error in creating the stars", err.Error())
+		return
+	} else {
+		game.stars = stars
+	}
+	if err := ebiten.RunGame(&game); err != nil {
 		log.Fatal(err)
 	}
 }
---
+++ mapGen/gameMap.go
@@ -0,0 +1,120 @@
+package mapgen
+
+import (
+	"errors"
+
+	"image/color"
+	"math/rand"
+)
+
+type Location struct {
+	Id            int
+	X             float64
+	Y             float64
+	ClusterId     int
+	IsClusterCore bool
+}
+
+type Cluster struct {
+	Core  Location
+	color color.RGBA
+}
+
+type MapGenConfigs struct {
+	MaxX                    float64
+	MaxY                    float64
+	StarCount               int
+	ClusterCount            int
+	StartRepulsionFactor    float64
+	ClusterAttractionFactor float64
+}
+
+// var gameMap []Location
+
+func InitMap(configs MapGenConfigs) ([]Location, []Cluster, error) {
+
+	if configs.ClusterCount > configs.StarCount {
+		return nil, nil, errors.New("the cluster count cant be greater than star count")
+	}
+
+	stars := initStarsAtRandomLocations(configs.StarCount, configs.MaxX, configs.MaxY)
+
+	// we will assume that the first N stars are cluster cores.
+	for i := 0; i < configs.ClusterCount; i++ {
+		stars[i].IsClusterCore = true
+	}
+
+	distanceGrid := getDistanceGrid(configs, stars)
+
+	// assign stars to clusters
+	updateStarClusters(stars, distanceGrid, configs.ClusterCount)
+	// movestars away from each other
+	// move stars towards cluster core
+
+	return stars, nil, nil
+}
+
+func updateStarClusters(stars []Location, distanceGrid [][]float64, clusterCount int) {
+
+	for i, star := range stars {
+		if i < clusterCount {
+			star.ClusterId = i
+		} else {
+			closestClusterCoreId := getClosestClusterCoreId(star, distanceGrid, stars[:clusterCount])
+			star.ClusterId = closestClusterCoreId
+		}
+	}
+}
+
+func getClosestClusterCoreId(star Location, distanceGrid [][]float64, clusterCores []Location) int {
+	clusterId := clusterCores[0].Id
+	minDist := distanceGrid[star.Id][0]
+
+	for _, coreStar := range clusterCores {
+		dist := distanceGrid[star.Id][coreStar.Id]
+		if dist < minDist {
+			minDist = dist
+			clusterId = coreStar.Id
+		}
+	}
+	return clusterId
+}
+
+func getDistanceGrid(configs MapGenConfigs, stars []Location) [][]float64 {
+	distanceGrid := make([][]float64, configs.StarCount)
+	for i := 0; i < configs.StarCount; i++ {
+		distanceGrid[i] = make([]float64, configs.StarCount)
+	}
+
+	for i := 0; i < configs.StarCount; i++ {
+		for j := i; j < configs.StarCount; j++ {
+			distanceGrid[i][j] = getDistSqr(stars[i], stars[j])
+			distanceGrid[j][i] = distanceGrid[i][j]
+		}
+	}
+	return distanceGrid
+}
+
+func getDistSqr(location1, location2 Location) float64 {
+	dx := location1.X - location2.X
+	dy := location1.Y - location2.Y
+
+	return dx*dx + dy*dy
+}
+
+func initStarsAtRandomLocations(starCount int, maxX float64, maxY float64) []Location {
+	locations := make([]Location, starCount)
+
+	for i := 0; i < starCount; i++ {
+
+		loc := Location{
+			X:             rand.Float64() * maxX,
+			Y:             rand.Float64() * maxY,
+			ClusterId:     -1,
+			IsClusterCore: false,
+		}
+
+		locations[i] = loc
+	}
+	return locations
+}
